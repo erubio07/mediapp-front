9fbee751e2f4530179a102efa3ca4dd7b7534b15
erubio07 <erubio07@gmail.com>
2024-12-22 13:16:54 +0000

    funciones basicas en suscription

--- src/Components/Subscription/Subscription.jsx
+++ src/Components/Subscription/Subscription.jsx
@@ -5,32 +5,58 @@ import Form from "react-bootstrap/Form";
 import styles from "./Subscription.module.css";
 
 const Subscription = () => {
-    return (
-        <div className={styles.container}>
-            <div className={styles.sub}>
+  const [input, setInput] = useState({
+    name: "",
+    surname: "",
+    email: "",
+    username: "",
+    password: "",
+  });
+  // console.log(input);
+
+  const handleChange = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.sub}>
+        <h4 className={styles.title}>Suscribirse</h4>
         <Form>
           <Form.Group className="mb-3">
             <Form.Label>Nombre: </Form.Label>
             <Form.Control
               type="string"
+              name="name"
               placeholder="Nombre"
               className={styles.input}
+              value={input.name}
+              onChange={handleChange}
             />
           </Form.Group>
           <Form.Group className="mb-3">
             <Form.Label>Apellido: </Form.Label>
             <Form.Control
               type="string"
+              name="surname"
               placeholder="Apellido"
               className={styles.input}
+              value={input.surname}
+              onChange={handleChange}
             />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Mail: </Form.Label>
             <Form.Control
               type="mail"
+              name="email"
               placeholder="Email"
               className={styles.input}
+              value={input.email}
+              onChange={handleChange}
             />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
@@ -41,8 +67,11 @@ const Subscription = () => {
             <Form.Label>Username: </Form.Label>
             <Form.Control
               type="string"
+              name="username"
               placeholder="Username"
               className={styles.input}
+              value={input.username}
+              onChange={handleChange}
             />
           </Form.Group>
 
@@ -50,8 +79,11 @@ const Subscription = () => {
             <Form.Label>Password: </Form.Label>
             <Form.Control
               type="password"
+              name="password"
               placeholder="Password"
               className={styles.input}
+              value={input.password}
+              onChange={handleChange}
             />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
@@ -61,9 +93,9 @@ const Subscription = () => {
             Siguiente
           </Button>
         </Form>
-            </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Subscription;
