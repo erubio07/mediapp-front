5a5607c82904cad56263153953ba362b6bc8f2c7
Ezequiel Rubio <erubio07@gmail.com>
2025-01-31 00:38:23 -0300

    primer documento de jabe

--- src/Components/CreateDocuments/CreateDocuments.jsx
+++ src/Components/CreateDocuments/CreateDocuments.jsx
@@ -2,25 +2,53 @@ import React, { useState } from "react";
 import axios from "axios";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import styles from "./CreateDocuments.module.css"
+import styles from "./CreateDocuments.module.css";
 
 const CreateDocuments = () => {
-  const [nombre, setNombre] = useState("");
-  console.log(nombre);
+  const [input, setInput] = useState({
+    expediente: "",
+    number: "",
+    date: "",
+    hour: "",
+    start: "",
+    end: "",
+    nextDate: "",
+    requirente: {
+      name: "",
+      dni: "",
+    },
+    requerido: {
+      name: "",
+      dni: "",
+    },
+  });
+  console.log(input);
 
-  const handleName = (e) => {
-    setNombre(e.target.value);
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+
+    setInput((prevState) => {
+      // Verificamos si el campo pertenece a 'requirente' o 'requerido'
+      if (name.includes("requirente.") || name.includes("requerido.")) {
+        const [parent, field] = name.split(".");
+        return {
+          ...prevState,
+          [parent]: {
+            ...prevState[parent],
+            [field]: value,
+          },
+        };
+      }
+
+      return { ...prevState, [name]: value };
+    });
   };
 
   const generateDocument = async () => {
     try {
-      const response = await axios.post(
-        "https://molecular-ferdinande-ezequielrubio-c5ad57aa.koyeb.app/fill",
-        { nombre },
-        {
-          responseType: "blob", // Especificar que la respuesta es un blob
-        }
-      );
+      const response = await axios.post("http://localhost:3000/fill", input, {
+        responseType: "blob", // Especificar que la respuesta es un blob
+      });
 
       // Crear un blob a partir del archivo recibido
       const blob = new Blob([response.data], {
@@ -31,7 +59,7 @@ const CreateDocuments = () => {
       const url = window.URL.createObjectURL(blob);
       const link = document.createElement("a");
       link.href = url;
-      link.download = `documento_${nombre || "generico"}.docx`; // Usar el nombre ingresado o un valor genérico
+      link.download = `documento_${"generico"}.docx`; // Usar el nombre ingresado o un valor genérico
       document.body.appendChild(link);
       link.click();
       link.remove();
@@ -46,7 +74,16 @@ const CreateDocuments = () => {
   const handleCreate = async (e) => {
     e.preventDefault();
     try {
-      if (!nombre) alert("Se debe ingresar un nombre para proceder");
+      if (
+        !input.expediente ||
+        !input.number ||
+        !input.date ||
+        !input.requirente.name ||
+        !input.requirente.dni ||
+        !input.requerido.name ||
+        !input.requerido.dni
+      )
+        alert("Se debe ingresar un nombre para proceder");
       await generateDocument();
     } catch (error) {
       console.log(error.message);
@@ -58,17 +95,78 @@ const CreateDocuments = () => {
       <h1 className={styles.title}>Crear Documentos</h1>
       <Form onSubmit={handleCreate} className={styles.form}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Nombre: </Form.Label>
+          <Form.Label>Carátula de Expediente: </Form.Label>
           <Form.Control
-            type="nombre"
-            placeholder="Ingrese un Nombre"
-            value={nombre}
-            onChange={handleName}
+            type="text"
+            name="expediente"
+            placeholder="Ingrese una Carátula"
+            value={input.expediente}
+            onChange={handleInput}
             className={styles.formControl}
           />
-          {/* <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text> */}
+          <Form.Label>Número: </Form.Label>
+          <Form.Control
+            type="text"
+            name="number"
+            placeholder="Ingrese el Número del Expediente"
+            value={input.number}
+            onChange={handleInput}
+            className={styles.formControl}
+          />
+          <Form.Label>Fecha: </Form.Label>
+          <Form.Control
+            type="text"
+            name="date"
+            placeholder="Ingrese la Fecha"
+            value={input.date}
+            onChange={handleInput}
+            className={styles.formControl}
+          />
+          <Form.Label>Hora: </Form.Label>
+          <Form.Control
+            type="text"
+            name="hour"
+            placeholder="Ingrese la Hora"
+            value={input.hour}
+            onChange={handleInput}
+            className={styles.formControl}
+          />
+          <Form.Label>Nombre del Requirente: </Form.Label>
+          <Form.Control
+            type="text"
+            name="requirente.name"
+            placeholder="Ingrese el nombre del Requirente"
+            value={input.requirente.name}
+            onChange={handleInput}
+            className={styles.formControl}
+          />
+          <Form.Label>DNI Requirente: </Form.Label>
+          <Form.Control
+            type="text"
+            name="requirente.dni"
+            placeholder="Ingrese el DNI del Requirente"
+            value={input.requirente.dni}
+            onChange={handleInput}
+            className={styles.formControl}
+          />
+          <Form.Label>Nombre del Requerido: </Form.Label>
+          <Form.Control
+            type="text"
+            name="requerido.name"
+            placeholder="Ingrese el nombre del Requerido"
+            value={input.requerido.name}
+            onChange={handleInput}
+            className={styles.formControl}
+          />
+          <Form.Label>DNI: </Form.Label>
+          <Form.Control
+            type="text"
+            name="requerido.dni"
+            placeholder="Ingrese el DNI del Requerido"
+            value={input.requerido.dni}
+            onChange={handleInput}
+            className={styles.formControl}
+          />
         </Form.Group>
 
         {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
